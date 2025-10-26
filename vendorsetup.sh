2e0da8fcf8808982286469b5a7f29c4fa91e97b4
Ganjaboy3942 <97562444+Ganjaboy3942@users.noreply.github.com>
2025-10-26 17:11:28 -0400

    Update vendorsetup.sh
    Omni to twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_X306F-user
-add_lunch_combo omni_X306F-userdebug
-add_lunch_combo omni_X306F-eng
+add_lunch_combo twrp_X306F-user
+add_lunch_combo twrp_X306F-userdebug
+add_lunch_combo twrp_X306F-eng
